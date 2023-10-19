5851060cb43a6f052855c82f8d7ce615f17131b3
key <30455795+keyhsw@users.noreply.github.com>
2023-10-19 18:51:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import os
-os.system('streamlit run examples/react_web_demo.py --server.port 7860')
+from subprocess import Popen, PIPE, STDOUT
+
+def exe_shell(command: str, shell: str = '/bin/bash'):
+    return Popen(command, stdout=PIPE, stderr=STDOUT, shell=True, executable=shell)
+
+exe_shell('streamlit run examples/react_web_demo.py --server.port 7860')
+# os.system('streamlit run examples/react_web_demo.py --server.port 7860')
