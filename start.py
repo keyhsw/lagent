52f3b91257193d917544b3270bd70ffdc306f7b2
key <30455795+keyhsw@users.noreply.github.com>
2023-10-26 16:55:25 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,3 @@
 import os
 os.system('mim install mmcv')
-os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
+os.system('streamlit run examples/react_web_demo.py --server.address=0.0.0.0 --server.port 7860')
